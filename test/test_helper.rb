a76114772c983f2a7f78e0938117da63dd49d83b
Marko Anastasov <marko@renderedtext.com>
2014-05-03 23:28:10 +0200

    Use minitest instead of test/unit.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require 'test/unit'
+require 'minitest/autorun'
 require 'active_support/test_case'
 require 'rails/all'
 require 'rails/generators'
